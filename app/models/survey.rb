66a74dc80309cb2e85763f5ce2dca80bc71f48f0
Andrew Carr <aacarr5@gmail.com>
2016-04-26 15:54:15 -0500

    Modify industries

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,6 @@ class Survey < ActiveRecord::Base
 	GENDERS = ["Non-gender Conforming","Gender-fluid","Trans-Femme","Trans-Masc","Gender-free","Female","Male",'Other']
 	RESPONSES = %w(Yes No Maybe)
 	RACES = ["Black", "Asian","Mixed-Race","Latino/Latina", "Native-American","White",'Other']
-	INDUSTRIES = %w(Real-estate Healthcare/Medicine Law Technology Marketing/Advertising Finance Non-profit Retail Food-industry Government Transportation Education Agriculture Mining Construction Arts/Entertainment Social-Services)
+	INDUSTRIES = %w(Real-estate Healthcare/Medicine Law Technology Marketing/Advertising Finance Non-profit Retail Food-industry Government Transportation Education Agriculture Mining Construction Arts/Entertainment Social-Services Other)
 
 end
