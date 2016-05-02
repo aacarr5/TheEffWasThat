a1542e493d1844d18efe701ced7bdcca52b74e3b
Andrew Carr <aacarr5@gmail.com>
2016-05-02 16:14:44 -0500

    Create reroute for bad survey submissions

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -7,13 +7,17 @@ class SurveysController < ApplicationController
 	end
 
 	def create
-		Survey.create!(survey_params)
-		redirect_to surveys_path
+		@survey = Survey.new(survey_params)
+		if @survey.valid?
+			@survey.save
+			redirect_to surveys_path
+		else
+			render 'new'
+		end
 	end
-
 
 	private
 		def survey_params
-			params.require(:survey).permit(:zipcode,:gender,:race,:industry,:age,:email,:faced_micoragressions,:seen_microaggressions)
+			params.require(:survey).permit(:zipcode,:gender,:race,:industry,:age,:email,:faced_microaggressions,:seen_microaggressions)
 		end
 end
