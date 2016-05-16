0247621cea7e0d3cf0f18328576634bd75b597a3
Andrew Carr <aacarr5@gmail.com>
2016-05-15 20:22:29 -0500

    Formate survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
 
-	GENDERS = ["'Other',Non-gender Conforming","Gender-fluid","Agender","Trans-Femme","Trans-Masc","Female","Male",]
+	GENDERS = ["Other","Non-gender Conforming","Gender-fluid","Agender","Trans-Femme","Trans-Masc","Female","Male"]
 	RESPONSES = %w(Yes No Maybe)
 	RACES = ['Other',"Black", "Asian","Indian","Mixed-Race","Latino/Latina", "Native-American","White"]
 	INDUSTRIES = %w(Real-estate Healthcare/Medicine Law Technology Marketing/Advertising Finance Non-profit Retail Food-industry Government Transportation Education Agriculture Mining Construction Arts/Entertainment Social-Services Other)
