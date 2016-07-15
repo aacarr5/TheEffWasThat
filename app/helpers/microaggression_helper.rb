af9b18d4369e7fb9543174d94adbca5e625894bc
Andrew Carr <aacarr5@gmail.com>
2016-07-15 13:02:25 -0500

    Add micro model

---
+++ app/helpers/microaggression_helper.rb
@@ -0,0 +1,2 @@
+module MicroaggressionHelper
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 	RACES = ['Other',"Black", "Asian","Indian","Mixed-Race","Latino/Latina", "Native-American","White"]
 	INDUSTRIES = %w(Real-estate Healthcare/Medicine Law Technology Marketing/Advertising Finance Non-profit Retail Food-industry Government Transportation Education Agriculture Mining Construction Arts/Entertainment Social-Services Other)
 
+	has_many :microaggressions
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
